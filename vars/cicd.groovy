a50251893bc3f512ef9dd61b0d6d3073e95d563a
Sainathrao <saisainath09@gmail.com>
2026-01-01 13:56:53 +0530

    Add testing function to cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,3 +9,7 @@ def buildartifacts(){
 def depolyment(foldername, IP, filename){
   sh "scp /var/lib/jenkins/workspace/${foldername}/webapp/target/webapp.war ubuntu@${IP}:/var/lib/tomcat10/webapps/${filename}.war"
 }
+
+def Tesing(foldername){
+  sh "java -jar /var/lib/jenkins/workspace/${foldername}/testing.jar"
+}
